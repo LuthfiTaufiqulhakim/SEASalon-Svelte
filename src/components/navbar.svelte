6a9acab676080b9cdf099187ce44c917a8a98738
luthfit64 <luthfit64@gmail.com>
2024-06-28 22:46:54 +0000

    init

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,6 +1,6 @@
 <nav class="navbar relative-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">SEASalon</a>
+    <a class="navbar-brand" href="/">SEASalon</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
       <div class="navbar-toggler-icon">
         <span></span>
@@ -16,22 +16,22 @@
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="/">Link</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Dropdown
             </a>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="/">Action</a></li>
+              <li><a class="dropdown-item" href="/">Another action</a></li>
               <li>
                 <hr class="dropdown-divider">
               </li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="/">Something else here</a></li>
             </ul>
           </li>
         </ul>
